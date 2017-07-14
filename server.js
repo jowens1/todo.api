dd4a2331f7a3acd953bc8b78c3f06cdcd62b8d69
jowens <justinowens328@gmail.com>
2017-07-14 17:58:14 -0400

    package-lock.json commit

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use('/api', require('./routes/task'));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
