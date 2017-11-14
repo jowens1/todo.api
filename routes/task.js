837017a9360bd6395dc7767ebcfc46d604de4949
jowens1 <justinowens328@gmail.com>
2017-11-14 00:23:21 -0500

    updated route approach

--- routes/task.js
+++ routes/task.js
@@ -1,76 +1,71 @@
-// routes/task.js
-'use strict';
-
-// dependencies
-const express = require('express');
 const Task = require('../models/tasks');
-const router = express.Router();
 
-// route definitions here...
+module.exports = app => {
+	app
+		.route('/todos')
+		.post((req, res) => {
+			const task = new Task({
+				name: req.body.name,
+				note: req.body.note,
+			});
 
-// routes ending with /todos
-router.route('/todos')
-    .post((req, res) => {
-        const task = new Task({
-            name: req.body.name,
-            note: req.body.note
-        });
+			task.save(err => {
+				if (err) {
+					return res.send(err);
+				}
 
-        task.save(err => {
-            if (err) {
-                return res.send(err);
-            }
+				return res.json({ message: 'New task created' });
+			});
+		})
+		.get((req, res) => {
+			Task.find({})
+				.sort({ createdAt: -1 })
+				.exec((err, task) => {
+					if (err) {
+						return res.send(err);
+					}
 
-            return res.json({message: 'New task created'});
-        });
-    })
-    .get((req, res) => {
-        Task.find({}).sort({createdAt: -1})
-            .exec((err, task) => {
-                if (err) {
-                    return res.send(err);
-                }
+					return res.json(task);
+				});
+		});
 
-                return res.json(task);
-            });
-    });
+	// routes starting with /todos/:id
+	app
+		.route('/todos/:id')
+		.get((req, res) => {
+			Task.findById(req.params.id, (err, task) => {
+				if (err) {
+					return res.send(err);
+				}
 
-// routes starting with /todos/:id
-router.route('/todos/:id')
-    .get((req, res) => {
-        Task.findById(req.params.id, (err, task) => {
-            if (err) {
-                return res.send(err)
-            }
+				return res.json(task);
+			});
+		})
+		.put((req, res) => {
+			Task.findByIdAndUpdate(
+				req.params.id,
+				{
+					// fix to this portion to not require passing the name and note in order to avoid nullifying fields
+					name: req.body.name,
+					note: req.body.note,
+					completed: req.body.completed,
+				},
+				err => {
+					if (err) {
+						return res.send(err);
+					}
 
-            return res.json(task);
-        })
-    })
-    .put((req, res) => {
-        Task.findByIdAndUpdate(req.params.id, {
-                // fix to this portion to not require passing the name and note in order to avoid nullifying fields
-                name: req.body.name,
-                note: req.body.note,
-                completed: req.body.completed
-            },
-            err => {
-                if (err) {
-                    return res.send(err);
-                }
+					return res.json({ message: 'Task updated successfully' });
+				}
+			);
+		})
+		.delete((req, res) => {
+			Task.remove({ _id: req.params.id }, err => {
+				if (err) {
+					return res.send(err);
+				}
 
-                return res.json({message: 'Task updated successfully'});
-            }
-        )
-    })
-    .delete((req, res) => {
-        Task.remove({_id: req.params.id}, err => {
-            if(err){
-                return res.send(err);
-            }
-
-            return res.json({message: 'Task has been removed'});
-        });
-    });
-
-
-module.exports = router;
+				return res.json({ message: 'Task has been removed' });
+			});
+		});
+};
--- server.js
+++ server.js
@@ -1,34 +1,23 @@
-'use strict';
-
-// dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
-// create the express app
 const app = express();
 
 // configure the body-parser to accept urlencoded bodies to json data
-app.use(bodyParser.urlencoded({ extended : true })).use(bodyParser.json());
-
-// get an instance of the express router
-const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: true })).use(bodyParser.json());
 
 // register all routers all routes are prefixed with /api
 app.use('/api', require('./routes/task'));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+app.use(cors());
+require('./routes/task')(app);
 // set the port
 const port = parseInt(process.env.PORT, 10) || 8000;
 
 // start the server
 const server = app.listen(port, () => {
-    console.log(`App is running at: localhost:${server.address().port}`);
+	console.log(`App is running at: localhost:${server.address().port}`);
 });
 
 // database connections
